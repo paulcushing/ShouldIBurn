6db64ad968721dae0803e69b09483b97f4525737
Paul Cushing <pcushing@gmail.com>
2021-04-06 08:59:56 -0600

    Style updates

--- components/begin.js
+++ components/begin.js
@@ -5,14 +5,17 @@ export const Begin = (props) => {
         props.setHaveUserLoc(true)
     }
     return (
-        <div className="container max-w-lg px-4 py-24 mx-auto text-left md:max-w-none text-center">
+        <div className="container max-w-lg px-4 py-8 mx-auto text-left md:max-w-none text-center">
             <h1 className="text-5xl font-extrabold leading-10 tracking-tight text-left text-gray-900 text-center sm:leading-none md:text-6xl lg:text-7xl">
                 <span className="inline md:block">Should I Burn?</span>
             </h1>
             <div className="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-lg text-center lg:text-lg">
+                <p>Check the conditions to see if you should burn weeds or have a fire on your property.</p>
+            </div>
+            <div className="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-lg text-center lg:text-lg">
                 <button
                     onClick={() => props.setPermissionGranted(true)}
-                    className="text-3xl font-extrabold leading-10 tracking-tight text-white rounded-full bg-indigo-600 p-6 text-center"
+                    className="text-3xl font-extrabold leading-10 tracking-tight text-white rounded-full bg-indigo-600 px-6 py-2 text-center"
                 >
                     Locate Me
                 </button>
@@ -23,7 +26,7 @@ export const Begin = (props) => {
             <div className="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-lg text-center lg:text-lg">
                 <input
                     id="location"
-                    className="p-6 border-solid border-2 border-indigo-600"
+                    className="p-6 border-solid border-2 rounded-md border-indigo-600"
                     type="text"
                     value={props.userLocation}
                     onChange={(e) => props.setUserLocation(e.target.value)}
@@ -31,7 +34,7 @@ export const Begin = (props) => {
                 />
                 <button
                     onClick={() => saveUserLocation()}
-                    className="text-2xl leading-10 tracking-tight text-white rounded-full bg-indigo-600 px-3 m-2 text-center"
+                    className="text-xl leading-10 tracking-tight text-white rounded-full bg-indigo-600 px-4 m-2 text-center"
                 >
                     Go
                 </button>
