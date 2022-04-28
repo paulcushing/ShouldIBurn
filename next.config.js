e0e09da2f26872e4fbdd97f94a0dbba3289ee979
paulcushing <pcushing@gmail.com>
2022-04-28 10:51:42 -0600

    Repairing build with Sentry

--- next.config.js
+++ next.config.js
@@ -3,24 +3,56 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-const { withSentryConfig } = require('@sentry/nextjs');
+const { withSentryConfig } = require('@sentry/nextjs')
 
-const moduleExports = {
-  // Your existing module.exports
-};
+const {
+    NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
+    SENTRY_ORG,
+    SENTRY_PROJECT,
+    SENTRY_AUTH_TOKEN,
+    NODE_ENV,
+    VERCEL_GIT_COMMIT_SHA,
+} = process.env
+
+const withWorkbox = require('next-with-workbox')
+
+const moduleExports = withWorkbox({
+    workbox: {
+        //swSrc: "worker.js",
+    },
+    productionBrowserSourceMaps: true,
+    env: {
+        // Make the COMMIT_SHA available to the client so that Sentry events can be
+        // marked for the release they belong to. It may be undefined if running
+        // outside of Vercel
+        NEXT_PUBLIC_COMMIT_SHA: VERCEL_GIT_COMMIT_SHA,
+    },
+    webpack: (config, options) => {
+        // Define an environment variable so source code can check whether or not
+        // it's running on the server so we can correctly initialize Sentry
+        config.plugins.push(
+            new options.webpack.DefinePlugin({
+                'process.env.NEXT_IS_SERVER': JSON.stringify(
+                    options.isServer.toString()
+                ),
+            })
+        )
+        return config
+    },
+})
 
 const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
+    // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    // the following options are set automatically, and overriding them is not
+    // recommended:
+    //   release, url, org, project, authToken, configFile, stripPrefix,
+    //   urlPrefix, include, ignore
 
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
+    silent: true, // Suppresses all logs
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options.
+}
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions)
--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,30 @@
 import Head from 'next/head'
 import { useEffect } from 'react'
-import { init } from '../utils/sentry'
-import { Workbox } from "workbox-window";
+import { Workbox } from 'workbox-window'
 import '../styles/globals.css'
-
-init()
 
 function MyApp({ Component, pageProps, err }) {
     useEffect(() => {
         if (
-          !("serviceWorker" in navigator) ||
-          process.env.NODE_ENV !== "production"
+            !('serviceWorker' in navigator) ||
+            process.env.NODE_ENV !== 'production'
         ) {
-          console.warn("No ServiceWorker present - Progressive Web App support is disabled");
-          return;
+            console.warn(
+                'No ServiceWorker present - Progressive Web App support is disabled'
+            )
+            return
         }
-    const wb = new Workbox("sw.js", { scope: "/" });
-        wb.register();
-      }, []);
+        const wb = new Workbox('sw.js', { scope: '/' })
+        wb.register()
+    }, [])
 
     return (
         <>
             <Head>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0, width=device-width"
+                />
             </Head>
             <Component {...pageProps} err={err} />
         </>
